8cae3dee9617c2fd2e3eb8c2cf665e7f67eea023
alex_kart <ugo-lok@yandex.ru>
2020-02-10 17:36:00 +0300

    Чистый шаблон для сборки. Готов к работе.

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+/*
+ * Third party
+ */
+//= ../../node_modules/jquery/dist/jquery.min.js
+//= ../../node_modules/popper.js/dist/umd/popper.min.js
+//= ../../node_modules/bootstrap/dist/js/bootstrap.min.js
+// *** Castom
+//= partials/app.js
